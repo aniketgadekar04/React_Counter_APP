d877ddd5a7f6f30c7e0bbaa6e2b6c7dd1952a5e6
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2025-12-05 17:21:39 +0530

    Project Done

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,16 @@ function App() {
         Increment && Decrement
       </div>
       <div className="bg-[#fff] flex justify-center gap-12 text-[#344151] py-3 text-[25px] rounded-sm">
-        <button onClick={decHandler}>+</button>
-        <div>{count}</div>
-        <button onClick={incHandler}>-</button>
+        <button
+          onClick={decHandler}
+          className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
+          -
+        </button>
+        <div className="font-bold gap-12 text-4xl">{count}</div>
+        <button onClick={incHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">+</button>
       </div>
-      <button onClick={reset}>Reset</button>
+      <button className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"  onClick={reset}>Reset</button>
     </div>
   );
 }
